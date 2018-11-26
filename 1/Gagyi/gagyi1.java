4a1a8ade3fdee20d31ac98550a8a3a25beb80bf7
Erdei Kristóf <36450903+kristoferdei@users.noreply.github.com>
2018-11-26 20:34:04 +0100

    Add files via upload

---
+++ 1/Gagyi/gagyi1.java
@@ -0,0 +1,18 @@
+//véges
+//javac gagy1.java
+//java gagyi
+
+public class gagyi1 {
+
+public static void main (String[]args) {
+
+    Integer x = -128;
+    Integer t = -128;
+
+    System.out.println (x);
+    System.out.println (t);
+
+    while (x <= t && x >= t && t != x);
+    
+    }
+}
---
+++ 1/Gagyi/gagyi2.java
@@ -0,0 +1,18 @@
+//végtelen
+//javac gagyi2.java
+//java gagyi2
+
+public class gagyi2 {
+
+public static void main (String[]args) {
+
+    Integer x = -129;
+    Integer t = -129;
+
+    System.out.println (x);
+    System.out.println (t);
+
+    while (x <= t && x >= t && t != x);
+
+    }
+} 
