e133d83789344a8761ea37db9309f910d7fc587a
Erdei Kristóf <36450903+kristoferdei@users.noreply.github.com>
2018-11-30 11:39:24 +0100

    Add files via upload

--- 5/Masolo-mozgato szemantika/const.cpp
+++ 5/Masolo-mozgato szemantika/const.cpp
@@ -7,39 +7,47 @@
 using namespace std;
 
 struct A {
-    A() {
-        cout << "Cons..." << endl;
-    }
+	A() {
+		cout << "Cons..." << endl;
+	}
 
-    A(const A & a1) {
-    cout << "Copy Cons..." << endl;
-    }
+	A(const A & a1) {
+		cout << "Copy Cons..." << endl;
+	}
 
-    A(A && a1) noexcept {
-    cout << "Move Cons..." << endl;
-    }
+	A& operator=(const A & a1) {
+		cout << "Copy Ass..." << endl;
+	}
 
-    ~A() {
-        cout << "Dest..." << endl;
-    }
+	A(A && a1) noexcept {
+		cout << "Move Cons..." << endl;
+	}
+
+	A& operator=(A && a1) noexcept {
+		cout << "Move Ass..." << endl;
+	}
+
+	~A() {
+		cout << "Dest..." << endl;
+	}
 };
 
 int main() {
-    {
-        vector<A> v1;
-        v1.push_back(A());  //calls Move
-    }
-    {
-        vector<A> v1;
-        A a2;
-        v1.push_back(a2);   //calls Copy
-    }
-    {
-        vector<A> v1;
-        A a3;
-        v1.push_back(move(a3));   //calls Move
-    }
-    
-    return 0;
-    
+	{
+		vector<A> v1;
+		v1.push_back(A());  //calls Move
+	}
+	{
+		vector<A> v1;
+		A a2;
+		v1.push_back(a2);   //calls Copy
+	}
+	{
+		vector<A> v1;
+		A a3;
+		v1.push_back(move(a3));   //calls Move
+	}
+
+	return 0;
+
 }
